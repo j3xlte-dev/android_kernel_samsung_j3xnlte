7e944602d80fc2ca2ef6761fa39f709d179b2f01
djeman <djeman@home.fr>
2017-05-28 05:33:16 +0200

    regulator: protect divide by zero

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -4037,6 +4037,3 @@ unlock:
  * not all module driver enable regulator before use at now,
  * and system regulators is not fixed, so always_on property is difficult to be set.
  */
-#if !defined(CONFIG_ARCH_SC)
-//late_initcall(regulator_init_complete);
-#endif
